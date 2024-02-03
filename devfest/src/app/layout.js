a7fe3fddbc121e6f3bb2d83b0171760824525c1e
Sushi <158117453+nlam0@users.noreply.github.com>
2024-02-03 16:46:20 -0500

    hotfix5

--- devfest/src/app/layout.js
+++ devfest/src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import Navbar from '../components/NavBar';
+import Navbar from '../components/navbar';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
